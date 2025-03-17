844be9772f97de94b908c1f88741909238f29635
Tishan Perera <tishanperera2001@gmail.com>
2025-03-17 20:40:06 +0530

    Add Instructor Assignment
    Instructor Assignment Model/Controller/Service/Rpository
    
    Assignment and Enrollment dashboard
    Instructor Assignment page
    Student Enrollment page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,8 +2,15 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
 import LoginPage from "./pages/LoginPage";  // Create LoginPage later
+<<<<<<< HEAD
 import RegisterPage from "./pages/RegisterPage"; 
 import UserManagerPage from "./pages/UserManagerPage";// Create RegisterPage later
+=======
+import RegisterPage from "./pages/RegisterPage"; // Create RegisterPage later
+import AssignmentDashboard from "./pages/AssignmentDashboard"; // Create AssignmentDashboard later
+import AssignInstructor from "./pages/AssignInstructor"; // Create AssignInstructor later
+import EnrollStudents from "./pages/EnrollStudents";
+>>>>>>> c7fc008 (Add Instructor Assignment)
 
 function App() {
   return (
@@ -11,7 +18,13 @@ function App() {
       <Route path="/" element={<LandingPage />} />  {/* Default Landing Page */}
       <Route path="/login" element={<LoginPage />} />
       <Route path="/register" element={<RegisterPage />} />
+<<<<<<< HEAD
       <Route path="/user-manager" element={<UserManagerPage />} />
+=======
+      <Route path="/assignmentdashboard" element={<AssignmentDashboard />} />
+      <Route path="/assign-instructors" element={<AssignInstructor />} />
+      <Route path="/enroll-students" element={<EnrollStudents />} />
+>>>>>>> c7fc008 (Add Instructor Assignment)
     </Routes>
   );
 }
