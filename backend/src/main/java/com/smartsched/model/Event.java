00101605e828b1cb38e46352bddd89da8581aefc
Kaveesha Nimanji <nimanjiliyanage@gmail.com>
2025-03-20 19:43:05 +0530

    update again

--- backend/src/main/java/com/smartsched/model/Event.java
+++ backend/src/main/java/com/smartsched/model/Event.java
@@ -18,5 +18,6 @@ public class Event {
     private String location;
     private String orgCommittee;
     private String description;
+    
 }
 
--- backend/src/main/java/com/smartsched/repository/EventRepository.java
+++ backend/src/main/java/com/smartsched/repository/EventRepository.java
@@ -3,6 +3,7 @@ package com.smartsched.repository;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import com.smartsched.model.Event;
 
+
 public interface EventRepository extends MongoRepository<Event, String> {
 
 }
