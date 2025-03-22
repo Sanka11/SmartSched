ac1f0d62b9d3995fa7c187da63a157609a933f72
Kaveesha Nimanji <nimanjiliyanage@gmail.com>
2025-03-22 10:35:16 +0530

    nimanji

--- backend/src/main/java/com/smartsched/model/Event.java
+++ backend/src/main/java/com/smartsched/model/Event.java
@@ -4,7 +4,8 @@ import lombok.Data;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-import java.time.LocalDateTime;
+import java.time.LocalDate;
+import java.time.LocalTime;
 
 @Data
 @Document(collection = "events")
@@ -12,12 +13,10 @@ public class Event {
     @Id
     private String eventId;
     private String eventName;
-    private LocalDateTime eventDate;
-    private LocalDateTime eventTime;
+    private LocalDate eventDate;  // stores only the date
+    private LocalTime eventTime;  // stores only the time
     private String eventMode;
     private String location;
     private String orgCommittee;
     private String description;
-    
 }
-
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,11 +6,17 @@ import AssignmentDashboard from "./pages/AssignmentDashboard"; // Create Assignm
 import AssignInstructor from "./pages/AssignInstructor"; // Create AssignInstructor later
 import EnrollStudents from "./pages/EnrollStudents";
 
-import CourseListPage from "./pages/CourseList";  //create CourseListPage
-import CourseForm from "./pages/CourseForm"; // create CourseForm
 
 import RegisterPage from "./pages/RegisterPage"; // Create RegisterPage later
 import UserManagerPage from "./pages/UserManagerPage";
+
+import EventForm from "./pages/EventForm"; //event form  page
+import EventList from "./pages/EventList";
+import EventDetails from "./pages/EventDetails";
+import CourseListPage from "./pages/CourseList"; //create CourseListPage
+import CourseForm from "./pages/CourseForm"; // create CourseForm
+import ViewCoursePage from "./pages/ViewCoursePage";
+
 
 
 
@@ -24,11 +30,15 @@ function App() {
       <Route path="/assignmentdashboard" element={<AssignmentDashboard />} />
       <Route path="/assign-instructors" element={<AssignInstructor />} />
       <Route path="/enroll-students" element={<EnrollStudents />} />
+      <Route path="/user-manager" element={<UserManagerPage />} />
+      <Route path="/createevent" element={<EventForm />} />
+      <Route path="/eventlist" element={<EventList />} />
+      <Route path="/view-event/:eventId" element={<EventDetails />} />
+      <Route path="/update-event/:eventId" element={<EventForm />} />{" "}
       <Route path="/courses" element={<CourseListPage />} />
       <Route path="/add-course" element={<CourseForm />} />
-      <Route path="/edit-course/:id" element={<CourseForm />} /> 
-      <Route path="/user-manager" element={<UserManagerPage />} />
-
+      <Route path="/update-course/:courseId" element={<CourseForm />} />
+      <Route path="/view-course/:courseId" element={<ViewCoursePage />} />
     </Routes>
   );
 }
